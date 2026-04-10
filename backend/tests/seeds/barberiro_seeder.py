2dc849ca1215f24cc7fe60c204b78d8e51e44f51
Vinicius Moreira <viniciusm1603@gmail.com>
2026-04-10 16:50:23 -0300

    Modificado a senha dos dados seeds para obedecer o schema

--- backend/tests/seeds/barberiro_seeder.py
+++ backend/tests/seeds/barberiro_seeder.py
@@ -1,24 +1,28 @@
-#josue inicio
+# josue inicio
 from app import db
 from app.models import Barbeiro
 from tests.seeds.base import BaseSeeder
 
+
 class BarbeiroSeeder(BaseSeeder):
     def obter_dados(self):
-        print('Obtendo dados dos barbeiros')
+        print("Obtendo dados dos barbeiros")
         return [
             Barbeiro(
-                nome='Marcos Navalha',
-                especialidade='Pigmentação', 
-                telefone='1197777', 
-                email='marcos@barbabyte.com', 
-                senha='123'),
+                nome="Marcos Navalha",
+                especialidade="Pigmentação",
+                telefone="1197777",
+                email="marcos@barbabyte.com",
+                senha="123@123",
+            ),
             Barbeiro(
-                nome='Carlos Tesoura', 
-                especialidade='Corte Clássico', 
-                telefone='1196666', 
-                email='carlos@barbabyte.com', 
-                senha='123'
-            )
+                nome="Carlos Tesoura",
+                especialidade="Corte Clássico",
+                telefone="1196666",
+                email="carlos@barbabyte.com",
+                senha="123@123",
+            ),
         ]
-#josue fim
+
+
+# josue fim
--- backend/tests/seeds/cliente_seeder.py
+++ backend/tests/seeds/cliente_seeder.py
@@ -1,24 +1,26 @@
-#josue inicio
+# josue inicio
 from app import db
 from app.models import Cliente
 from tests.seeds.base import BaseSeeder
 
+
 class ClientSeeder(BaseSeeder):
     def obter_dados(self):
-        print('Obtendo dados dos clientes')
+        print("Obtendo dados dos clientes")
         return [
             Cliente(
-                nome='João Silva',
-                telefone='11999999999',
-                email='joao@barbabyte.com',
-                senha='123'
+                nome="João Silva",
+                telefone="11999999999",
+                email="joao@barbabyte.com",
+                senha="123@123",
             ),
             Cliente(
-                nome='Maria Oliveira',
-                telefone='11988888888',
-                email='maria@barbabyte.com',
-                senha='123'
-            )   
-
+                nome="Maria Oliveira",
+                telefone="11988888888",
+                email="maria@barbabyte.com",
+                senha="123@123",
+            ),
         ]
-#josue fim
+
+
+# josue fim
